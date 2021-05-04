49af809e2ee4aa2a652380bcf5aaf26d0e3bac7f
Marko Plavljanin <mplavljanin@hotmail.com>
2021-05-04 22:17:41 +0200

    added a new python file

---
+++ permutations.py
@@ -0,0 +1,21 @@
+def permutation(string, start, end):
+    # Prints the permutations
+    if start == end - 1:
+        print(string)
+    else:
+        for current in range(start, end):
+            # Fixing a character
+            x = list(string)
+            temp = x[start]
+            x[start] = x[current]
+            x[current] = temp
+
+            # Recursively calling function permutation() for rest of the characters
+            permutation("".join(x), start + 1, end)
+
+
+string = input('Enter a string: ')
+n = len(string)
+print("All the permutations of the string are: ")
+permutation(string, 0, n)
+
