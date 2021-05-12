07fe580e5ad54a6133655a434646bc6d3c03950c
mp199 <79169207+mp199@users.noreply.github.com>
2021-05-12 19:53:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ for x in range(1, 6):
     for y in range(1, 6):
         print('x', end='')
     print()
+
+numbers = [2, 4, 1, 6, 8, 2, 1, 4, 9, 3, 5, 6, 2]
+frequency = {}
+ordered_frequency = []
+for n in numbers:
+    if n not in frequency:
+        frequency[n] = 1
+    else:
+        frequency[n] += 1
+for k, v in frequency.items():
+    ordered_frequency.append((k, v))
+ordered_frequency.sort(reverse=True)
+print(ordered_frequency)
